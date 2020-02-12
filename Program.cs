4aa3c024b5dfe679fc3a60298190a87657d4e540
justinsorensen2 <@gmail.com>
2020-02-12 17:21:25 -0500

    Completed explorer mode.

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Shuffler
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //create a var suit as a list with 4 items - 1 for each suit of cards
+      var suit = new List<string>() { "of Clubs", "of Diamonds", "of Hearts", "of Spades" };
+      //create a var rank as a list with 13 items - 1 for each rank
+      var rank = new List<string>() { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+      //create a new var deck as a list with no members
+      var deck = new List<string>();
+      //for loop to create deck by adding ranks to suits
+      for (var i = 0; i < suit.Count; i++)
+      {
+        for (var n = 0; n < rank.Count; n++)
+        {
+          var cardName = rank[n] + " " + suit[i];
+          deck.Add(cardName);
+        }
+      }
+      //create a list to contain the cards as they are shuffled and drawn
+      var shuffle = new List<string>();
+      //create a while loop to draw cards randomly from the deck
+      for (var j = 51; j > 0; j--)
+      {
+        //create a random number generator
+        Random rand1 = new Random();
+        var v = rand1.Next(1, 52);
+        //add drawn card to shuffled deck
+        shuffle.Add(deck[v]);
+      }
+      //display top card
+      Console.WriteLine($"{shuffle[0]} is the top card in the shuffled deck.");
+      //ask user if they would like to see next card
+      Console.WriteLine("Would you like to display the next card in the shuffled deck? Valid entries are yes or no.");
+      //create variable from user's input
+      var nextCard = Console.ReadLine().ToLower();
+      //error correction on inputs
+      if (nextCard != "yes" && nextCard != "no")
+      {
+        Console.WriteLine("That is not a valid entry. Please try again.");
+        nextCard = Console.ReadLine().ToLower();
+        if (nextCard == "yes")
+        {
+          Console.WriteLine($"{shuffle[1]} is the second card in the shuffled deck.");
+          Console.WriteLine("Thank you. Have a nice day!");
+        }
+        else
+        {
+          Console.WriteLine("Thank you. Have a nice day!");
+        }
+      }
+      else if (nextCard == "yes")
+      {
+        Console.WriteLine($"{shuffle[1]} is the second card in the shuffled deck.");
+        Console.WriteLine("Thank you. Have a nice day!");
+      }
+      else
+      {
+        Console.WriteLine("Thank you. Have a nice day!");
+      }
+      {
+
+      }
+
+    }
+  }
+}
