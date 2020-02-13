cb47b783bfead4fbfa2a72778c75e24b199e5dd5
justinsorensen2dotnet <justinsorensen2@gmail.com>
2020-02-12 21:59:04 -0500

    Attempted adventure mode - failed.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,12 @@ namespace Shuffler
   {
     static void Main(string[] args)
     {
+      //ask players 1 and 2 to enter their names
+      //store player names in var player1 and player2
+      Console.WriteLine("Player 1, please enter your name.");
+      var player1 = Console.ReadLine();
+      Console.WriteLine("Player 2, please enter your name.");
+      var player2 = Console.ReadLine();
       //create a var suit as a list with 4 items - 1 for each suit of cards
       var suit = new List<string>() { "of Clubs", "of Diamonds", "of Hearts", "of Spades" };
       //create a var rank as a list with 13 items - 1 for each rank
@@ -34,38 +40,74 @@ namespace Shuffler
         //add drawn card to shuffled deck
         shuffle.Add(deck[v]);
       }
-      //display top card
-      Console.WriteLine($"{shuffle[0]} is the top card in the shuffled deck.");
-      //while loop to repeat, ask user if they would like to see next card
-      //end loop when we either run out of cards or user says no (nested for)
+      //create new lists playerHand1 and playerHand2
+      var playerHand1 = new List<string>();
+      var playerHand2 = new List<string>();
+      //for loop to repeat, ask user if they would like to see next card
+      //end loop when we either run out of cards or user says no
 
       for (var l = 51; l > 0; l--)
       {
-        Console.WriteLine("Would you like to display the next card in the shuffled deck? Valid entries are yes or no.");
-        //create variable from user's input
-        var nextCard = Console.ReadLine().ToLower();
-        //variable to hold truth val, while loop to continue showing cards
-        //error correction on inputs
-        if (nextCard != "yes" && nextCard != "no")
+        if (l % 2 != 0)
         {
-          Console.WriteLine("That is not a valid entry. Please try again.");
-          nextCard = Console.ReadLine().ToLower();
+          //deal top card to player1
+          Console.WriteLine($"{player1} you have been dealt the {shuffle[l]}.");
+          playerHand1.Add(shuffle[l]);
+          Console.WriteLine($"{player2} would you like to be dealt the next card in the shuffled deck? Valid entries are yes or no.");
+          //create variable from user's input
+          var nextCard = Console.ReadLine().ToLower();
+          //variable to hold truth val, while loop to continue showing cards
+          //error correction on inputs
+          if (nextCard != "yes" && nextCard != "no")
+          {
+            Console.WriteLine("That is not a valid entry. Please try again.");
+            nextCard = Console.ReadLine().ToLower();
+          }
+          else if (nextCard == "yes")
+          {
+            l--;
+            Console.WriteLine($"{shuffle[l]} is the next card in the shuffled deck.");
+            playerHand2.Add(shuffle[l]);
 
-        }
-        else if (nextCard == "yes")
-        {
-          Console.WriteLine($"{shuffle[1]} is the next card in the shuffled deck.");
-
+          }
+          else
+          {
+            l = 0;
+            Console.WriteLine("Thank you. Have a nice day!");
+          }
         }
         else
         {
-          l = 0;
-          Console.WriteLine("Thank you. Have a nice day!");
+          Console.WriteLine($"{player2} you have been dealt the {shuffle[l]}.");
+          playerHand2.Add(shuffle[l]);
+          Console.WriteLine($"{player1} would you like to be dealt the next card in the shuffled deck? Valid entries are yes or no.");
+          //create variable from user's input
+          var nextCard = Console.ReadLine().ToLower();
+          //variable to hold truth val, while loop to continue showing cards
+          //error correction on inputs
+          if (nextCard != "yes" && nextCard != "no")
+          {
+            Console.WriteLine("That is not a valid entry. Please try again.");
+            nextCard = Console.ReadLine().ToLower();
+          }
+          else if (nextCard == "yes")
+          {
+            l--;
+            Console.WriteLine($"{shuffle[l]} is the next card in the shuffled deck.");
+            playerHand1.Add(shuffle[l]);
+
+          }
+          else
+          {
+            l = 0;
+            Console.WriteLine("Thank you. Have a nice day!");
+          }
+
+
+
         }
 
-
       }
-
     }
   }
 }
